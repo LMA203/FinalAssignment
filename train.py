d1b197b25158d492e17f0e9fb601f25bbbc1c5a5
Luc Manders <l.manders@student.tue.nl>
2024-04-10 11:59:09 +0200

    lr 0.01

--- train.py
+++ train.py
@@ -48,7 +48,7 @@ def main(args):
     model = Model().cuda()
 
     # define optimizer and loss function (don't forget to ignore class index 255)
-    learning_rate = 0.005
+    learning_rate = 0.01
     num_epochs = 30
     criterion = nn.CrossEntropyLoss(ignore_index=255)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
