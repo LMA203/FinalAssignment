e09a08c51133ccff54f70384c276f6fca4962f8d
Luc Manders <l.manders@student.tue.nl>
2024-04-18 09:58:55 +0200

    delete dropout

--- model.py
+++ model.py
@@ -6,23 +6,23 @@ import torch.nn.init as init
 """
 
 class Model(nn.Module):
-    def __init__(self, dropout_rate=0.2):
+    def __init__(self):
         super().__init__()
 
         """ Encoder """
-        self.e1 = encoder_block(3, 64, dropout_rate=dropout_rate)
-        self.e2 = encoder_block(64, 128, dropout_rate=dropout_rate)
-        self.e3 = encoder_block(128, 256, dropout_rate=dropout_rate)
-        self.e4 = encoder_block(256, 512, dropout_rate=dropout_rate)
+        self.e1 = encoder_block(3, 64)
+        self.e2 = encoder_block(64, 128)
+        self.e3 = encoder_block(128, 256)
+        self.e4 = encoder_block(256, 512)
 
         """ Bottleneck """
-        self.b = conv_block(512, 1024, dropout_rate=dropout_rate)
+        self.b = conv_block(512, 1024)
 
         """ Decoder """
-        self.d1 = decoder_block(1024, 512, dropout_rate=dropout_rate)
-        self.d2 = decoder_block(512, 256, dropout_rate=dropout_rate)
-        self.d3 = decoder_block(256, 128, dropout_rate=dropout_rate)
-        self.d4 = decoder_block(128, 64, dropout_rate=dropout_rate)
+        self.d1 = decoder_block(1024, 512)
+        self.d2 = decoder_block(512, 256)
+        self.d3 = decoder_block(256, 128)
+        self.d4 = decoder_block(128, 64)
 
         """ Classifier """
         self.outputs = nn.Conv2d(64, 19, kernel_size=1, padding=0)
@@ -60,12 +60,11 @@ class Model(nn.Module):
 
 
 class conv_block(nn.Module):
-    def __init__(self, in_c, out_c,dropout_rate):
+    def __init__(self, in_c, out_c):
         super().__init__()
 
         self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(out_c)
-        self.dropout = nn.Dropout(dropout_rate)
 
         self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(out_c)
@@ -76,7 +75,6 @@ class conv_block(nn.Module):
         x = self.conv1(inputs)
         x = self.bn1(x)
         x = self.relu(x)
-        x = self.dropout(x)
 
         x = self.conv2(x)
         x = self.bn2(x)
@@ -89,10 +87,10 @@ class conv_block(nn.Module):
     Here the number of filters doubles and the height and width half after every block.
 """
 class encoder_block(nn.Module):
-    def __init__(self, in_c, out_c, dropout_rate):
+    def __init__(self, in_c, out_c):
         super().__init__()
 
-        self.conv = conv_block(in_c, out_c, dropout_rate)
+        self.conv = conv_block(in_c, out_c)
         self.pool = nn.MaxPool2d((2, 2))
 
     def forward(self, inputs):
@@ -107,11 +105,11 @@ class encoder_block(nn.Module):
     Here the number filters decreases by half and the height and width doubles.
 """
 class decoder_block(nn.Module):
-    def __init__(self, in_c, out_c, dropout_rate):
+    def __init__(self, in_c, out_c):
         super().__init__()
 
         self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0)
-        self.conv = conv_block(out_c+out_c, out_c, dropout_rate)
+        self.conv = conv_block(out_c+out_c, out_c)
 
     def forward(self, inputs, skip):
         x = self.up(inputs)
