9042ac9a3649c9c179e258fea2fa34062c352072
Luc Manders <l.manders@student.tue.nl>
2024-04-18 12:33:50 +0200

    change number of epochs

--- train.py
+++ train.py
@@ -49,7 +49,7 @@ def main(args):
 
     # define optimizer and loss function (don't forget to ignore class index 255)
     learning_rate = 0.005
-    num_epochs = 30
+    num_epochs = 22
     criterion = nn.CrossEntropyLoss(ignore_index=255)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     num_classes = 19
